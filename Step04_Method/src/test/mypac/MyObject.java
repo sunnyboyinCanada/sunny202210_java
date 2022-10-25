71df3f25f3fb5bbfd992758b59a911021bb69959
SunhyunKim <conestoga1214@gmail.com>
2022-10-25 12:23:35 +0900

    Step04_Method 프로젝트 추가함

---
+++ Step04_Method/src/test/mypac/MyObject.java
@@ -0,0 +1,15 @@
+package test.mypac;
+
+public class MyObject {
+	public void walk() {
+		System.out.println("걸음을 걸어요.");
+	}
+	
+	public int getNumber() {
+		return 10;
+	}
+	
+	public String getGreeting() {
+		return "안녕하세요";
+	}
+}
